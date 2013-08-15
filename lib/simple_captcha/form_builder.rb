4961b1fbb372126c8318135cd22f263f74451d1b
Oksana Dushenkovskaya <oksanka.blaze@gmail.com>
2013-08-15 14:43:00 +0300

    Changed Sprockets::Helpers to Sprockets::Rails::Helper
    There is no Sprockets::Helpers anymore for rails master.

--- lib/simple_captcha/form_builder.rb
+++ lib/simple_captcha/form_builder.rb
@@ -4,8 +4,7 @@ module SimpleCaptcha
       base.send(:include, SimpleCaptcha::ViewHelper)
       base.send(:include, SimpleCaptcha::FormBuilder::ClassMethods)
       base.send(:include, ActionView::Helpers)
-      base.send(:include, Sprockets::Helpers::RailsHelper)
-      base.send(:include, Sprockets::Helpers::IsolatedHelper)
+      base.send(:include, Sprockets::Rails::Helper)
       
       base.delegate :render, :session, :to => :template
     end
